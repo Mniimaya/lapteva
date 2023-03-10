aa3d1eefe92cb1206ea92cc1844996090bdbe232
Marina Generalova <93405011+Mniimaya@users.noreply.github.com>
2023-03-10 16:23:23 +0300

    форма тест

---
+++ telegram.php
@@ -0,0 +1,26 @@
+<?php
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$msg = $_POST['msg'];
+$token = "6148480120:AAF6mtrqSXIA09yol6-UYXsW8BB1JlV-Bro";
+$chat_id = "-835593712";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+$sendToTelegram2 = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram && $sendToTelegram2) {
+  header('Location: thanks.html');
+} else {
+  echo "Error";
+}
+?>
