d4ed4c3f3dae2132e165afeb4672ba0dd8403d88
Marina Generalova <93405011+Mniimaya@users.noreply.github.com>
2023-03-10 14:42:30 +0300

    intro

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 const buttonMenu = document.querySelector('.button-menu');
 const headerNavigation = document.querySelector('.header__navigation');
+const servicesContainer = document.querySelector('.services__list-container');
 
 function handlerMenu() {
     buttonMenu.addEventListener('click', function(e) {
@@ -9,4 +10,35 @@ function handlerMenu() {
     });
 }
 
+
+const servicesSliderSwiper = new Swiper(servicesContainer, {
+    slideClass: 'services__item',
+    wrapperClass: 'services__list',
+    spaceBetween: 50,
+    slidesPerView: "1",
+    slidesPerGroup: 1,
+    direction: 'horizontal',
+    breakpoints: {
+        1920: {
+            slidesPerView: 5,
+            spaceBetween: 25
+          },
+          1280: {
+          slidesPerView: 4,
+          spaceBetween: 20
+          },
+          770: {
+              slidesPerView: 3,
+              spaceBetween: 20
+          },
+          480: {
+          slidesPerView: 2,
+          spaceBetween: 15
+      },
+      320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+      }
+      },
+});
 handlerMenu();
